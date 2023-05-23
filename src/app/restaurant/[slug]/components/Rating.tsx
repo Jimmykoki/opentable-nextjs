f7537b872b295b159ae2372ef617580d36574095
Jimmykoki <jianlil2@uci.edu>
2023-05-23 01:13:00 -0700

    refactor(app): refactored components

---
+++ src/app/restaurant/[slug]/components/Rating.tsx
@@ -0,0 +1,11 @@
+export default function Rating() {
+  return (
+    <div className="flex items-end">
+      <div className="rating mt-2 flex items-center">
+        <p>****</p>
+        <p className="text-reg ml-3">4.9</p>
+      </div>
+      <div className="text-reg ml-4">600 Reviews</div>
+    </div>
+  );
+}
