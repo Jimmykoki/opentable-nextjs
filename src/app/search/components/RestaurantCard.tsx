46abb95d1ec8f8208caa8f809023eb4d9c6d8960
Jimmykoki <jianlil2@uci.edu>
2023-05-25 15:51:46 -0700

    feat(components): added query option for search

--- src/app/search/components/RestaurantCard.tsx
+++ src/app/search/components/RestaurantCard.tsx
@@ -1,28 +1,45 @@
 import Link from 'next/link';
+import { RestaurantCardType } from '@/app/page';
+import Price from '@/app/components/Price';
+import { calculateRivewRating } from 'utils/calculateReviewRating';
+import Stars from '@/app/components/Stars';
 
-export default function RestaurantCard() {
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: RestaurantCardType;
+}) {
+  const renderRatingText = () => {
+    const rating = calculateRivewRating(restaurant.reviews);
+    if (rating > 4) return 'Awesome';
+    else if (rating <= 4 && rating > 3) return 'Excellent';
+    else if (rating <= 3 && rating > 2) return 'Average';
+    else if (rating <= 2 && restaurant.reviews.length) return 'Poor';
+    else return 'No reviews';
+  };
+
   return (
-    <div className="border-b flex pb-5">
-      <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
-        className="w-44 rounded"
-      />
+    <div className="border-b flex pb-5 ml-5">
+      <img src={restaurant.main_image} alt="" className="w-44 h-36 rounded" />
       <div className="pl-5">
-        <h2 className="text-3xl">Asian Restaurant Collective</h2>
+        <h2 className="text-3xl">{restaurant.name}</h2>
         <div className="flex items-start">
-          <div className="flex mb-2">****</div>
-          <p className="ml-2 text-sm">Awesome</p>
+          <div className="flex mb-2">
+            <Stars reviews={restaurant.reviews} />
+          </div>
+          <p className="ml-2 text-sm">{renderRatingText()}</p>
         </div>
         <div className="mb-9">
           <div className="font-light flex text-reg">
-            <p className="mr-4">$$$</p>
-            <p className="mr-4">Chinese</p>
-            <p className="mr-4">Toronto</p>
+            <Price price={restaurant.price} />
+            <p className="mr-4 capitalize">{restaurant.cuisine.name}</p>
+            <p className="mr-4 capitalize">{restaurant.location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
-          <Link href="/restaurant/milestone-grill">View more information</Link>
+          <Link href={`/restaurant/${restaurant.slug}`}>
+            View more information
+          </Link>
         </div>
       </div>
     </div>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,20 +1,112 @@
-import Link from 'next/link';
-import NavBar from '@/app/components/NavBar';
 import Header from './components/Header';
 import SearchSideBar from './components/SearchSideBar';
 import RestaurantCard from './components/RestaurantCard';
+import { PRICE, PrismaClient } from '@prisma/client';
 
-export default function Search() {
+const prisma = new PrismaClient();
+export interface Region {
+  name: string;
+}
+export interface Cuisine {
+  name: string;
+}
+interface SearchParams {
+  location?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
+
+const fetchRestaurantByLocation = (searchParams: SearchParams) => {
+  const where: any = {};
+
+  if (searchParams.location) {
+    const location = {
+      name: {
+        equals: searchParams.location.toLowerCase(),
+      },
+    };
+    where.location = location;
+  }
+  if (searchParams.cuisine) {
+    const cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase(),
+      },
+    };
+    where.cuisine = cuisine;
+  }
+  if (searchParams.price) {
+    const price = {
+      equals: searchParams.price,
+    };
+    where.price = price;
+  }
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+    reviews: true,
+  };
+
+  return prisma.restaurant.findMany({
+    where,
+    select,
+  });
+};
+
+const fetchRegion = async (): Promise<Region[]> => {
+  const regions = await prisma.location.findMany({
+    select: {
+      name: true,
+    },
+  });
+  return regions;
+};
+
+const fetchCuisine = async (): Promise<Cuisine[]> => {
+  const cuisines = await prisma.cuisine.findMany({
+    select: {
+      name: true,
+    },
+  });
+  return cuisines;
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const restaurants = await fetchRestaurantByLocation(searchParams);
+  console.log(searchParams);
+  const regions = await fetchRegion();
+  const cuisines = await fetchCuisine();
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         {/** SEARCH SIDE BAR */}
-        <SearchSideBar />
+        <SearchSideBar
+          regions={regions}
+          cuisines={cuisines}
+          searchParams={searchParams}
+        />
         {/** SEARCH SIDE BAR */}
         <div className="w-5/6">
           {/** RESTAURANT CARD */}
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map((restaurant) => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))
+          ) : (
+            <p className="test-light font-bold text-2xl">
+              Sorry, No restaurants found in this area
+            </p>
+          )}
           {/** RESTAURANT CARD */}
         </div>
       </div>
